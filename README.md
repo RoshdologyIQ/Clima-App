80778182024b4618a83baddf7cda4a994e8de647
roshdology <@Bdallah123>
2021-03-23 18:39:11 +0200

    Clima-Flutter Application

---
+++ README.md
@@ -0,0 +1,5 @@
+# climaapplication
+
+an application which tells you what is the climate
+
+I made this project alone without any help I just cloned it from app brewery.
---
+++ android/app/src/main/java/com/roshdologyiq/climaapplication/MainActivity.java
@@ -0,0 +1,6 @@
+package com.roshdologyiq.climaapplication;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
